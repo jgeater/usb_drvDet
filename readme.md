7e2b90dfd6d264542f62fbdd9f031ec3703f7958
James Geater <jgeater@outlook.com>
2017-03-13 14:34:18 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+usb_drvDet 1.0
+Written by jgeau in c# requires .net 3.0 or higher
+
+usb_drvDet.exe is just a simple tool that displays a dialog box asking the user to remove the USB drive if one is found on the machine.
+
+The message box is always on top so the user cannt send it behind other apps.
+
+It doesn not require admin rights
+
+
---
+++ usb_drvDet/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace usb_drvDet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drives = DriveInfo.GetDrives();
+            int USBDrv = 1;
+            while (USBDrv ==1 )
+            {
+                USBDrv = 0; //set to 0 just before checking
+
+                //for each drive look and see if it removable
+                foreach (var drive in drives)
+                {
+                    if (drive.DriveType == DriveType.Removable)
+                    {
+                        //found a usb drive so shohw a message that always on top and set the varible USBDrv to 1
+                        MessageBox.Show("Please remove the the USB drive and Click OK to Continue", "USB Drive Detected", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
+                        USBDrv = 1;
+                    }
+                }
+            }
+            Environment.Exit(0);
+        }
+    }
+}
